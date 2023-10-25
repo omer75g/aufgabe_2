fbdb062009ea0cd66980cb71e028e5d1fb5e2254
alice <alice@example.com>
2023-10-25 18:36:51 +0200

    new motivating phrases

--- text.py
+++ text.py
@@ -1,3 +1,12 @@
+from random import randint
+
+
 def text_1():
-    return "Super awesome text generation"
+    random_text = randint(0,3)
+    awesome_text = ['I didn’t fall down. I did attack the floor, though.',
+                    'I am nobody. Nobody is perfect. I am perfect.',
+                    'I’m never late. The others are just too early!',
+                    'There’s no “we” in fries.']
+
+    return awesome_text[random_text]
 
