c4eff721057fe2c9443fa6b792a29221b04f7ec1
alice <alice@example.com>
2025-01-29 18:53:45 +0100

    Fix: Korrektur von Apostrophen

--- text.py
+++ text.py
@@ -2,11 +2,12 @@ from random import randint
 
 
 def text_1():
-    random_text = randint(0,3)
+    random_text = randint(0,4)
     awesome_text = ['I didn’t fall down. I did attack the floor, though.',
                     'I am nobody. Nobody is perfect. I am perfect.',
                     'I’m never late. The others are just too early!',
-                    'There’s no “we” in fries.']
+                    'There’s no “we” in fries.',
+                    'I’m not sleeping, I’m resting my eyelids.']
 
     return awesome_text[random_text]
 
